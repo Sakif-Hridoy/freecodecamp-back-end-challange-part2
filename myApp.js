b11fa540370a7be70dd5466e40c3fa8e7680e397
sakif hridoy <silenticebreaker@gmail.com>
2022-08-27 04:01:53 +0600

    hello express corrected

--- myApp.js
+++ myApp.js
@@ -2,9 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-app.get(function(req, res) {
-    res.send('Hello Express');
+app.get('/', (req, res) => {
+    res.send('Hello Express')
   })
+  
 
 
 
